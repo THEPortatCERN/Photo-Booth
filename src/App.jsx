0b60071a19c79e3adb54f96e8f4ff33074e3294f
eddie ruxton <ae.ruxton@gmail.com>
2022-04-11 16:33:42 +0100

    basic styling added, image data accessed

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,22 @@ function App() {
 
   const webRef = useRef(null)
 
+  const onCapture = () => {
+    console.log('image campture', webRef.current.getScreenshot())
+  }
+
+  const onSaveImg = () => {
+    // post data to save.php
+  }
 
   return (
-    <>
-      <Webcam ref={webRef} />
-    </>
+    <div className="app">
+      <Webcam ref={webRef} className='webcam'/>
+      <div className="controls">
+      <button className="btn" onClick={onCapture}>CAPTURE</button>
+      <button className="btn" onClick={onSaveImg}>SAVE</button>
+      </div>
+    </div>
   );
 }
 
